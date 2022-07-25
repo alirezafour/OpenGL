755cb40583fdaf7c37689f6873a10b5f502dc8fc
alireza/arshGame <alirezafour@gmail.com>
2022-07-25 23:49:08 +0300

    debugging and drowing with opengl macro for display message old version

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,6 +1,137 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+// break point on error with VS only
+#define ASSERT(x) if (!(x)) __debugbreak();
+
+// Error Checking with x macro
+#define GLCall(x) GLClearError();\
+x;\
+ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+static void GLClearError()
+{
+	while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+	while (GLenum error = glGetError())
+	{
+		std::cout << "[OpenGL Error] : ( " << error << " ): " << function <<
+			" " << file << ":" << line << std::endl; // you can make your own function to translate this error code to proper error
+		return false;
+	}
+	return true;
+}
+
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+	std::ifstream stream(filepath);
+
+	enum class ShaderType
+	{
+		NONE = -1,
+		VERTEXT = 0,
+		FRAGMENT = 1
+	};
+
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+			{
+				// set mode to vertex
+				type = ShaderType::VERTEXT;
+			}
+			else if (line.find("fragment") != std::string::npos)
+			{
+				// set mode to fragment
+				type = ShaderType::FRAGMENT;
+			}
+		}
+		else
+		{
+			switch (type)
+			{
+			case ShaderType::NONE:
+				break;
+			case ShaderType::VERTEXT:
+				ss[0] << line << '\n';
+				break;
+			case ShaderType::FRAGMENT:
+				ss[1] << line << '\n';
+				break;
+			default:
+				break;
+			}
+
+			//ss[(int)type] << line << '\n';
+		}
+	}
+
+	return { ss[0].str(), ss[1].str() };
+}
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	GLCall(glShaderSource(id, 1, &src, nullptr));
+	GLCall(glCompileShader(id));
+
+	// Error handling in shader
+	int result;
+	GLCall(glGetShaderiv(id, GL_COMPILE_STATUS, &result));
+	if (result == GL_FALSE)
+	{
+		int length;
+		GLCall(glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length));
+		char* message = (char*)malloc(length * sizeof(char));
+		GLCall(glGetShaderInfoLog(id, length, &length, message));
+
+		std::cout << "Failed to compile" <<
+			(type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
+		std::cout << message << std::endl;
+
+		GLCall(glDeleteShader(id));
+		return 0;
+	}
+
+	return id;
+}
+
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	GLCall(glAttachShader(program, vs));
+	GLCall(glAttachShader(program, fs));
+	GLCall(glLinkProgram(program));
+	GLCall(glValidateProgram(program));
+
+	GLCall(glDeleteShader(vs)); // it's already program so we can delete it
+	GLCall(glDeleteShader(fs));
+
+	return program;
+}
 
 int main(void)
 {
@@ -30,17 +161,44 @@ int main(void)
 	// Print opengl version we are using
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
+	/* define buffer to begin with before game loop */
+
+	float positions[] = {
+		-0.5f, -0.5f, // 0
+		 0.5f, -0.5f, // 1
+		 0.5f,  0.5f, // 2
+		-0.5f,  0.5f  // 3
+	};
+
+	unsigned int indices[] = { // index buffer
+		0, 1, 2,
+		2, 3, 0
+	};
+
+	unsigned int buffer;
+	GLCall(glGenBuffers(1, &buffer)); // your buffer will be accessed by this int
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer)); //need to bind our buffer to be drawn if you bind something else it draw the other thing
+	GLCall(glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW)); // static means it's a hint that doesn't going to be change and will be draw every frame or something
+	
+	GLCall(glEnableVertexAttribArray(0)); // you should enable doesn't matter after next line or not cuz opengl is state machine way as long as binded it works
+	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0));
+
+	unsigned int ibo; // using index buffer 
+	GLCall(glGenBuffers(1, &ibo));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
+
+	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
+	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+	GLCall(glUseProgram(shader));
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
+		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
-// 		glBegin(GL_TRIANGLES);
-// 		glVertex2f(-0.5f, -0.5f);
-// 		glVertex2f(0.f, 0.5f);
-// 		glVertex2f(0.5f, -0.5f);
-// 		glEnd();
+		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr)); // usually you use this for draw call
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
@@ -48,6 +206,8 @@ int main(void)
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
+
+	GLCall(glDeleteProgram(shader));
 
 	glfwTerminate();
 	return 0;
